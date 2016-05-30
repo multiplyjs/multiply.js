4696390058ef96383c6b00168935d5ca3ec9e596
StigCoder04 <stevenhe04@live.com>
2016-05-29 18:28:15 -0700

    Moved unfinished IQR

--- src/multiply.js
+++ src/multiply.js
@@ -14,27 +14,13 @@ var Math = {
     return round;
   },
 
-  iqr: function(returnType, dataSet) {
-    var rT = returnType;
-    var q1;
-    var q2;
-    var q3;
-    var iqr;
-    var completeArray;
-
-    if (rT == Math.q1) {
-      return q1;
-    } else if (rt == Math.q2) {
-      return q2;
-    } else if (rt == Math.q3) {
-      return q3;
-    } else if (rt == Math.iqr) {
-      return iqr;
-    } else if (rt == Math.array) {
-      completeArray = [q1, q2, q3, q4, iqr];
-      return completeArray;
-    }
-  },
+  addDigits: function(base, added) {
+    baseStr = base.toString();
+    addedStr = added.toString();
+    finalRes = base + addedStr;
+    final = parseInt(finalRes);
+    return final;
+  }
 
   mad: function(dataSet) {
     var currentDataSet = dataSet;
@@ -64,13 +50,6 @@ var Math = {
     return mean;
   },
 
-  median: function(dataSet) {
-
-  },
-
-  mode: function(dataSet) {
-
-  },
 
   range: function(dataSet) {
     var high;
